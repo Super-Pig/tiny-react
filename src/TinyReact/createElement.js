6d7a4fecc1a0e58223dcb6c457505a9c6084ca0a
彭淦 <penggan@penggans-MacBook-Air.local>
2021-07-19 20:33:33 +0800

    构建虚拟dom

---
+++ src/TinyReact/createElement.js
@@ -0,0 +1,21 @@
+export default function createElement(type, props, ...children) {
+  const childElements = [].concat(...children).reduce((result, child) => {
+    if (typeof child !== 'boolean' && child !== null) {
+      if (child instanceof Object) {
+        result.push(child)
+      } else {
+        result.push(createElement('text', {
+          textContent: child
+        }))
+      }
+    }
+
+    return result
+  }, [])
+
+  return {
+    type,
+    props: Object.assign({ children: childElements }, props),
+    children: childElements
+  }
+}
