64fb4b63aa36fec109f47970c7f3b4937fef5e67
彭淦 <penggan@penggans-MacBook-Air.local>
2021-07-20 17:39:35 +0800

    diff 节点类型不同，新旧节点替换

--- src/TinyReact/diff.js
+++ src/TinyReact/diff.js
@@ -3,6 +3,7 @@ import mountComponent from "./mountComponent";
 import isFunction from "./isFunction";
 import updateTextNode from "./updateTextNode";
 import updateNodeElement from "./updateNodeElement";
+import createDOMElement from "./createDOMElement";
 
 export default function diff(virtualDOM, container, oldDOM) {
   const oldVirtualDOM = oldDOM && oldDOM._virtualDOM
@@ -10,6 +11,12 @@ export default function diff(virtualDOM, container, oldDOM) {
   // 判断 oldDOM 是否存在
   if (!oldDOM) {
     mountElement(virtualDOM, container)
+  } else if(oldVirtualDOM && oldVirtualDOM.type !== virtualDOM.type && !isFunction(virtualDOM)){
+    // 普通元素组件 && 组件类型不相同
+    const newElement = createDOMElement(virtualDOM)
+
+    // 新节点替换旧节点
+    oldDOM.parentNode.replaceChild(newElement, oldDOM)
   } else if (oldVirtualDOM && oldVirtualDOM.type === virtualDOM.type) {
     // 新旧 virtualDOM 类型相同
     if (virtualDOM.type === 'text') {
